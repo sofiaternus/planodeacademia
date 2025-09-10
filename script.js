8286289a8cac61e33f860003d68a53652a26db6d
sofiaternus <sofisofisodi@gmail.com>
2025-09-10 08:38:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ function validarCadastro() {
                 localStorage.setItem("usuarioCadastrado", "true");
                 window.location.href = "plan.html";
             } else {
-                // Exibe o modal avisando que precisa preencher os campos
                 const modal = new bootstrap.Modal(document.getElementById('modalAviso'));
                 modal.show();
             }
         }
-
-        // Bloqueia acesso 
         if (window.location.pathname.includes("plan.html") && localStorage.getItem("usuarioCadastrado") !== "true") {
             window.location.href = "index.html"; 
+
         }
